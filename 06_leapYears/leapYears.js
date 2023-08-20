ad782c5b13953fe0491d6a13a3fa06e110bf1889
SudoLudens <sudoludens@gmail.com>
2023-08-20 18:54:36 -0500

    Complete exercise

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,19 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    let result;
+  
+	if (year % 100 == 0) {
+        if (year % 400 == 0) {
+            result = true;
+        } else {
+            result = false;
+        }
+    } else if (year % 4 == 0) {
+        result = true;
+    } else {
+        result = false;
+    }
 
+	return result;
 };
 
 // Do not edit below this line
