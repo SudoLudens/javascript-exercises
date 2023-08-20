42afbc02a3500c4017482bf66238270501e088d2
SudoLudens <sudoludens@gmail.com>
2023-08-20 16:06:00 -0500

    Complete exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,15 @@
-const reverseString = function() {
+const reverseString = function(string) { 
+    // Declare new array
+    let array = [];
+
+    // Loop over each character of string
+    // Add each character to a new array
+    for (let i = 0; i < string.length; i++) {
+      array.unshift(string[i]);
+    }
+    
+    // Return joined array
+    return array.join("");
 
 };
 
