08a092fe30aeda066d73e066a1f48def5bb0e0f8
SudoLudens <sudoludens@gmail.com>
2023-08-20 17:32:28 -0500

    Complete exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...moreArgs) {
+	for (let i = 0; i < moreArgs.length; i++) {
+    let elementToRemove = array.indexOf(moreArgs[i]);
+    if (elementToRemove >= 0) {
+    	array.splice(elementToRemove, 1);
+    }
+    
+  }
+  return array;
 };
 
 // Do not edit below this line
